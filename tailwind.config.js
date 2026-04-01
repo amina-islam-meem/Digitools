443b8dfbbe4c61a0534e1d56ac764ced43208595
amina-islam-meem <amina.meem28@gmail.com>
2026-04-01 13:19:42 +0600

    chore: initial setup with vite, tailwind and daisyui

---
+++ tailwind.config.js
@@ -0,0 +1,6 @@
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: { extend: { colors: { primary: "#4f46e5" } } },
+  plugins: [require("daisyui")],
+  daisyui: { themes: ["light"] },
+}
